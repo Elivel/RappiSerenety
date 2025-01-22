e3c89bbadc75261e6d5d03c855c23f8f1f707e6b
Elivel <elivel88@gmail.com>
2025-01-21 19:39:18 -0500

    Add files via upload

--- RappiSerenety/src/test/java/stepsdefinitions/ApiStepDefinitions.java
+++ RappiSerenety/src/test/java/stepsdefinitions/ApiStepDefinitions.java
@@ -2,22 +2,19 @@ package stepsdefinitions;
 
 import io.cucumber.java.en.*;
 import net.serenitybdd.rest.SerenityRest;
-
 import static org.hamcrest.Matchers.equalTo;
-
 public class ApiStepDefinitions {
-    private String endpoint = "https://shazam.p.rapidapi.com/songs/v2/get-details?id=1217912247&l=en-US";
+    private String endpoint;
 
     @Given("I send a GET request to {string}")
     public void iHaveTheEndpoint(String url) {
+        this.endpoint = "https://shazam.p.rapidapi.com/songs/v2/get-details?id=1217912247&l=en-US";
         SerenityRest.given()
                 .header("x-rapidapi-key", "3b4c083e97mshed8b1fee588d1c2p1690bajsn14c29b303a29")
                 .header("x-rapidapi-host", "shazam.p.rapidapi.com")
                 .header("Content-Type", "application/json")
-                //.header("x-rapidapi-ua","RapidAPI-Playground")
-                //.header("specificMethodHeaders","[object Object]")
-                .get(url);
-       // Imprimir el código de respuesta para depuración
+                .get(endpoint);
+       // Imprimir el código de respuesta
         int actualStatusCode = SerenityRest.lastResponse().getStatusCode();
         System.out.println("Actual Response Code: " + actualStatusCode);
     }
@@ -29,7 +26,7 @@ public class ApiStepDefinitions {
     @Then("the \"id\" in the response data should be {string}")
     public void theIdInTheResponseDataShouldBe(String expectedId) {
         SerenityRest.then()
-                .body("data.id[0]", equalTo(expectedId));  // Verifica que el valor del campo 'id' sea 1217912247
+                .body("data.id[0]", equalTo(expectedId));
         System.out.println(SerenityRest.then().extract().body().asString());
     }
 }
--- RappiSerenety/target/test-classes/features/validate_post_response_code.feature
+++ RappiSerenety/target/test-classes/features/validate_post_response_code.feature
@@ -1,12 +1,9 @@
-Feature: Validate POST API response code
+Feature: Validate  API response code
+Background: Run api
+  Given I send a GET request to "https://shazam.p.rapidapi.com/songs/v2/get-details?id=1217912247&l=en-US"
 
   Scenario: Validate that the response code is 200
-    Given I have the endpoint "https://shazam.p.rapidapi.com/songs/v2/detect?timezone=America%2FChicago&locale=en-US"
-    And I have the request body
-    When I send a POST request
-    Then the response code should be 204
+    Then the response code should be 200
 
   Scenario: Validate that the 'id' field contains 1217912247
-    Given I also have another endpoint "https://shazam.p.rapidapi.com/songs/v2/get-details?id=1217912247&l=en-US"
-    When I send a GET request
-    Then the response should contain the 'id' field with value 1217912247
+    Then the "id" in the response data should be "1217912247"
